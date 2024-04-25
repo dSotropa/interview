6b423d326f84ec55bf6c60f7bde690e99a017776
Daniel Constantin Sotropa <dSotropa@users.noreply.github.com>
2024-04-25 13:28:48 +0300

    feat: first draft

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './auth/components/login/login.component';
+import { DashboardComponent } from './dashboard/components/dashboard/dashboard.component';
+import { NewProductComponent } from './dashboard/components/new-product/new-product.component';
+import { SalesComponent } from './dashboard/components/sales/sales.component';
+import { AuthGuard } from './auth/guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'login', title:"Login Page", component: LoginComponent },
+    { path: '', title:"Dashboard", component: DashboardComponent, canActivate:[AuthGuard] },
+    { path: 'new-product', title:"Dashboard", component: NewProductComponent, canActivate:[AuthGuard] },
+    { path: 'sales', title:"Sales", component: SalesComponent, canActivate:[AuthGuard] },
+
+
+];
